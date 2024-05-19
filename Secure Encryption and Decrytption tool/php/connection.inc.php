7887224231bca3e9cec6867df672354219c0d746
Thamira Herath <thamiraherath@yahoo.com>
2024-05-19 19:44:22 +0530

    smol changes ^^               \               U

--- Secure Encryption and Decrytption tool/php/connection.inc.php
+++ Secure Encryption and Decrytption tool/php/connection.inc.php
@@ -1,12 +1,16 @@
 <?php
+  session_start();
+  require 'PHPMailer/PHPMailerAutoload.php';
   $serverName = "localhost";
   $dBUsername = "root";
   $dBPassword = "";
   $dBName = "ppdsp";
- 
-  $con = mysqli_connect(  $serverName, $dBUsername, $dBPassword, $dBName );
+
+  $con = mysqli_connect(  $serverName, $dBUsername, $dBPassword, $dBName);
  
   if(!$con){
     die("Connection faild: ". mysqli_connect_error() );
   }
+
+  
 ?>
