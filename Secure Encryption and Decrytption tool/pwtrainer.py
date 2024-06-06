8b093a8193a80f98599e775154fc7f982871f839
Thamira Herath <99383284+ThamiraHerath@users.noreply.github.com>
2024-06-06 22:13:04 +0530

    Update pwtrainer.py
    Added the password vectorization and the test result storage location

--- Secure Encryption and Decrytption tool/pwtrainer.py
+++ Secure Encryption and Decrytption tool/pwtrainer.py
@@ -8,3 +8,25 @@ from sklearn.metrics import accuracy_score
 #loading the trainer data, a list of most common passwords
 
 data = pd.read_csv ('Secure-Encryption-and-Decryption-tool-/Secure Encryption and Decrytption tool/10-million-password-list-top-10000.txt')
+
+#splitting the dataset into featueres and tragets. 
+x = data ['password']
+y = data ['strength']
+
+vectorizeer = TfidVectorizeer (analyzer = 'char',ngram_range = (1,3))
+X_train, X_test, y_train, y_test = train_test_split(X_vectorized, y, test_size=0.2, random_state=42)
+
+# Train RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+
+# Make predictions on test set
+y_pred = clf.predict(X_test)
+
+# Evaluate model performance
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Save trained model
+joblib.dump(clf, 'password_strength_model.pkl')
+joblib.dump(vectorizer, 'password_vectorizer.pkl')
